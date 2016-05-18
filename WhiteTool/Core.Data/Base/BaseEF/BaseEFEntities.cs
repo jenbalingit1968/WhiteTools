c069a365d2e8651d2a30674ba9376b32474e2644
jenbalingit <jeneson.balingit@basecamptech.ph>
2016-05-18 23:15:03 +0800

    Setup BaseEFEntities in Core.Data

---
+++ WhiteTool/Core.Data/Base/BaseEF/BaseEFEntities.cs
@@ -0,0 +1,21 @@
+﻿using Core.Data.Interface;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Data.Base.BaseEF
+{
+    public class BaseEFEntities : IAuditable
+    {
+        public long ID { get; set; }
+
+        [Required]
+        public long CreatedBy { get; set; }
+
+        public long LastUpdatedBy { get; set; } 
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? LastUpdateDate { get; set; }
+    }
+}
---
+++ WhiteTool/Core.Data/Interface/IAuditable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data.Interface
+{
+    interface IAuditable
+    {
+         DateTime CreatedDate { get; set; }
+
+         DateTime? LastUpdateDate { get; set; }
+    }
+}
